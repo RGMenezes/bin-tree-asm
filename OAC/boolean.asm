9ce811cfb88b5c62237a19bb5e641c1166e61765
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2025-09-04 23:03:17 -0300

    fazendo exercicios

---
+++ OAC/boolean.asm
@@ -0,0 +1,125 @@
+org 0x7c00
+
+
+mov bh, 4
+
+
+mov al, 5
+mov bl, 1
+call sum
+add al, '0'
+mov ah, 0x0F
+mov di, 2080
+call print
+mov di, 2082     
+call check_result 
+
+
+mov al, 5
+mov bl, 1
+call subtr
+add al, '0'
+mov ah, 0x0F
+mov di, 2120
+call print
+mov di, 2122     
+call check_result 
+
+
+mov al, 1
+mov bl, 2
+call sum
+add al, '0'
+mov ah, 0x0F
+mov di, 2240
+call print
+mov di, 2242     
+call check_result 
+
+
+mov al, 1
+mov bl, 2
+call subtr
+add al, '0'
+mov ah, 0x0F
+mov di, 2280
+call print
+mov di, 2282     
+call check_result 
+
+loop:
+    jmp loop
+
+
+sum:
+    add al, bl
+    ret
+
+subtr:
+    sub al, bl
+    cmp al, 0
+    jge continue_subtr
+    neg al
+    call print_negative
+continue_subtr:
+    ret
+
+
+check_result:
+    push ax
+    push di
+
+    
+    
+    sub al, '0'
+    cmp al, bh      
+    
+    
+    je is_true      
+    
+    
+    jmp is_false
+
+is_true:
+    mov al, 'T'
+    mov ah, 0x0F
+    call print
+    pop di
+    pop ax
+    ret
+
+is_false:
+    mov al, 'F'
+    mov ah, 0x0F
+    call print
+    pop di
+    pop ax
+    ret
+
+print_negative:
+    push ax
+    push di
+    mov al, '-'
+    mov ah, 0x0F
+    mov di, 2278
+    call print
+    pop di
+    pop ax
+    ret
+
+print:
+    push ax
+    push bx
+    push es
+
+    mov bx, 0xB800
+    mov es, bx
+    pop bx
+    mov [es:di], ax
+
+    pop bx
+    pop ax
+    ret
+
+times 510-($-$$) db 0
+dw 0xAA55
